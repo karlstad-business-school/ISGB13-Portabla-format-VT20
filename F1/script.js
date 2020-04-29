91d9fe958cdb1bd33c327fe33a175c6977255a58
Pierre <pierre.sjoberg@kau.se>
2020-04-29 11:08:23 +0200

    Exempel F1

---
+++ F1/script.js
@@ -0,0 +1,61 @@
+window.addEventListener('load', minfunktion);
+
+
+
+// https://restcountries.eu/rest/v2/name/sweden
+function minfunktion(){
+
+    let container = document.getElementById('content');
+    let preloader = document.querySelector('#preloader');
+
+
+
+    window.fetch('https://restcountries.eu/rest/v2/name/sweden').then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        
+        let countryData = data[0];
+        console.log(countryData);
+
+        let card = document.createElement('div');
+        card.className = 'card';
+        card.style.maxWidth = '20rem';
+        container.appendChild(card);
+
+        let cardImage = document.createElement('img');
+        cardImage.className='card-img-top';
+        cardImage.src = countryData.flag;
+        card.appendChild(cardImage);
+
+        let cardBody = document.createElement('div');
+        cardBody.className = 'card-body';
+        card.appendChild(cardBody);
+
+        let cardTitle = document.createElement('h5');
+        cardTitle.className='card-title';
+        cardTitle.textContent = countryData.name;
+        cardBody.appendChild(cardTitle);
+
+        let capital = document.createElement('p');
+        capital.className = 'card-text';
+        capital.innerHTML = '<b>Capital:</b> ' + countryData.capital;
+        cardBody.appendChild(capital);
+
+        let area = document.createElement('p');
+        area.classList.add('card-text');
+        area.innerHTML = '<b>Area:</b> ' + countryData.area.toLocaleString() + ' km<sup>2</sup>';
+        cardBody.appendChild(area);
+
+        let population = document.createElement('p');
+        population.className = 'card-text';
+        population.innerHTML = '<b>Population:</b> ' + countryData.population.toLocaleString();
+        cardBody.appendChild(population)
+
+        preloader.classList.add('d-none');
+
+
+
+
+
+    });
+}
